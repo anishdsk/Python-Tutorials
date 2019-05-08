51f293a4cb02df9758cbae0d20a54129d3cda1e2
Anish Senthilkumar <anishdsk@yahoo.com>
2019-05-08 00:45:32 -0400

    lists update

--- lists.py
+++ lists.py
@@ -11,3 +11,26 @@ print(bicycles[0].title())
 print(bicycles[-1])
 # to return second to last item in list, type "-2" as compartment number
 print(bicycles[-2])
+
+# print statement using an element of a list
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+print()
+
+#second list manipulation example
+relatives = ['mom', 'dad', 'uncle']
+print(relatives[0].title() + ", " + relatives[1].title() + ", " + relatives[2].title())
+
+message = "Hello " + relatives[0]
+print(message)
+message = "Hello " + relatives[1]
+print(message)
+message = "Hello " + relatives[2]
+print(message)
+print()
+
+#modifying an element of a lists
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
