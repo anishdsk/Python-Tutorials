86c179dc84b63eed79801da566272f7ec6ec7330
Anish Senthilkumar <anishdsk@yahoo.com>
2019-05-08 00:47:43 -0400

    lists update

--- lists.py
+++ lists.py
@@ -29,7 +29,7 @@ message = "Hello " + relatives[2]
 print(message)
 print()
 
-#modifying an element of a lists
+#modifying an element of a list
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 motorcycles[0] = 'ducati'
