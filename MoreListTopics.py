bf76a92b36069ec7a18e7609452148f939a2ab4c
Anish Senthilkumar <anishdsk@yahoo.com>
2019-05-08 13:32:58 -0400

    Working with lists efficiently

---
+++ MoreListTopics.py
@@ -0,0 +1,6 @@
+# MORE TOPICS RELATED TO LISTS
+
+# using a for loop to loop through a list to print every element
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians: # magician is the loop variable that goes to every element in the list
+    print(magician) # prints each new list element 'magician' takes on as it goes through the list indices
