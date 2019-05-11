eb40f45041e564f78105de3d230f3f962d18f80a
Anish Senthilkumar <anishdsk@yahoo.com>
2019-05-11 16:38:43 -0400

    simple changes

--- MoreListTopics.py
+++ MoreListTopics.py
@@ -30,9 +30,9 @@ print(even_numbers)
 print()
 
 # another range() example
-squares =[]
+squares = []
 for value in range(1,11):
-    square =value*2
+    square = value*2
     squares.append(square)
 print(squares)
 print()
