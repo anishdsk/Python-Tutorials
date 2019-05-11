dc0b3a68681554fe53ef9af79b63b780f3a1371a
Anish Senthilkumar <anishdsk@yahoo.com>
2019-05-11 17:58:07 -0400

    final changes

--- if_statements_and_conditionals.py
+++ if_statements_and_conditionals.py
@@ -21,10 +21,22 @@ cars = ['audi', 'bmw', 'subaru', 'toyota']
 print('honda' in cars)
 print('bmw' in cars)
 print('subaru' not in cars)
+print()
 
-# if else
+# if-else
 age = 17
 if age >= 18:
     print("You are old enough to vote.")
 else:
     print("You are too young to vote")
+print()
+
+# if-elif-else
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5")
+else:
+    print("Your admission cost is $10")
