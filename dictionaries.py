f04e6472320efbd2b968209540b34508dd2367a0
Anish Senthilkumar <anishdsk@yahoo.com>
2019-05-11 17:58:22 -0400

    dictioanry tutorials

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+# DICTIONARIES
+
+# dictionaries are a collection of key-value pairs. Similar to maps in Java
+
+# simple dictionary
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
