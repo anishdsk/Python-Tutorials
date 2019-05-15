ece87a246ba82bb641aa6dba09df5975f03891e4
Anish Senthilkumar <anishdsk@yahoo.com>
2019-05-15 15:13:47 -0400

    modifying and adding elements to dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -2,8 +2,23 @@
 
 # dictionaries are a collection of key-value pairs. Similar to Java maps
 
-# simple dictionary
+# simple dictionary -> uses curly braces '{ }'
+# every key is connected to is connected to its value by a colon
+# individual key-value pairs are seperated by commas
 alien_0 = {'color': 'green', 'points': 5}
 
+# accessing individual values
 print(alien_0['color'])
 print(alien_0['points'])
+print()
+
+# pulls value of 'points' key
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+print()
+
+# add to the dictionary
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y-position'] = 25
+print(alien_0)
