b49b598dc801b28d6366de10d2e356844547486a
Anish Senthilkumar <anishdsk@yahoo.com>
2019-05-22 12:28:36 -0400

    Comment Changes

--- dictionaries.py
+++ dictionaries.py
@@ -8,16 +8,19 @@
 alien_0 = {'color': 'green', 'points': 5}
 
 # accessing individual values
+print("Accessing Individual Values:")
 print(alien_0['color'])
 print(alien_0['points'])
 print()
 
 # pulls value of 'points' key
+print("Pulling Value of a Key")
 new_points = alien_0['points']
 print("You just earned " + str(new_points) + " points!")
 print()
 
 # add to the dictionary
+print("Adding Values to a Dictionary")
 print(alien_0)
 alien_0['x_position'] = 0
 alien_0['y-position'] = 25
@@ -30,12 +33,15 @@ print(alien_0)
 print()
 
 # modifying an element of a dictionary
+print("Modifying an Element of a Dictionary:")
 print(alien_0['color'])
 alien_0['color'] = 'yellow'
 print(alien_0['color'])
+print()
 
 # removing key-value pairs
 # deleted key-value pairs are removed permanently
+print("Deleting Key-Value Pairs:")
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0)
 del alien_0['points']
@@ -49,6 +55,8 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python'
 }
+print("New Dictionary Example:")
+print(favorite_languages)
 print("sarah's favorite language is " + favorite_languages['sarah'].title())
 print()
 
@@ -59,6 +67,7 @@ user_0 = {
     'last': 'fermi'
 }
 # key, value are the names for the key-value pair contents but they could be any name
+print("Looping Through a Dictionary Using items() Method:")
 for key, value in user_0.items():
     print("\nKey:" + key)
     print('\nValue:' + value)
@@ -73,13 +82,14 @@ for name, language in favorite_languages.items():
     print(name.title() + "'s favorite language is " + language.title())
 print()
 
-# looping through only the keys in a  using keys() method
+# looping through only the keys using keys() method
 favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
     'edward': 'ruby',
     'phil': 'python'
 }
+print("Looping Through Keys only Using keys() Method:")
 for name in favorite_languages.keys(): # behaviour is the same even without .keys()
     print(name.title())
 print()
@@ -91,7 +101,8 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python'
 }
-friends = ['phil', 'sarah'] # keys with empty values
+friends = ['phil', 'sarah'] # list of keys with empty values
+print("Looping With Conditionals Example:")
 for name in favorite_languages.keys():
     print(name.title())
 
@@ -106,6 +117,7 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python'
 }
+print("Looping Through Keys in Order:")
 for name in sorted(favorite_languages.keys()):
     print(name.title() + ", thank you for taking the poll.")
 print()
@@ -117,6 +129,7 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python'
 }
+print("Printing Only the Values Using the values() Method:")
 print("The following languages have been mentioned:")
 for language in favorite_languages.values():
     print(language.title())
@@ -129,6 +142,7 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python'
 }
+print("Printing Values Using set() Method to Avoid Reprinting Repeating Values:")
 for language in set(favorite_languages.values()): # when set( ) is used it builds a set out of the unique contents so no repetitions
     print(language.title())
 print()
@@ -139,10 +153,12 @@ print()
 # make multiple dictionaries using range
 aliens = []
 # make 30 green aliens
+print("Make 30 Green Aliens(Dictionaries):")
 for alien_number in range(30):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 # output the first five aliens
+print("Output the First Five Aliens:")
 for alien in aliens[:5]:
         print(alien)
 print("...")
@@ -151,6 +167,7 @@ print("Total number of aliens: " + str(len(aliens)))
 print()
 
 # changing individual elements of the dictionary
+print("Changing Individual Elements of the Dictionary and Printing the First Five:")
 for alien in aliens[0:3]: # changes the first three elements/aliens
     if alien['color'] == 'green':
         alien['color'] = 'yellow',
@@ -171,11 +188,13 @@ pizza = {
     'crust': 'thick',
     'toppings': ['mushrooms', 'extra cheese'],
 }
+print("New Dictionary About Pizza Toppings:")
 # summarize the order
+print("Summarizing an Order:")
 print("you ordered a " + pizza['crust'] + "-crust pizza " + "with the following toppings:")
 for topping in pizza['toppings']:
     print("\t" + topping)
-
+print()
 # dictionaries in dictionaries
 users = {
     'aeinstein': {
@@ -189,7 +208,10 @@ users = {
         'location': 'paris',
     },
 }
-
+print("New 'Users' Dictionary Involving Nesting:")
+print(users)
+print()
+print("User Info:")
 for username, user_info in users.items(): # 'username' holds keys and 'user_info' holds the values
     print("\nUsername: " + username)
     full_name = user_info['first'] + " " + user_info['last']
