ca6900168194566c2781742b47b68dedcf7a76c4
Anish Senthilkumar <anishdsk@yahoo.com>
2019-06-04 14:30:25 -0400

    '*' asterisk operator added

--- functionModuleCallingFile.py
+++ functionModuleCallingFile.py
@@ -3,6 +3,7 @@
 # importing the module file
 import functionModule
 
+
 # importing just a/the specific function of the module file and using 'as' to give the function an alias name
 from functionModule import make_pizza as  mp
 
@@ -10,3 +11,6 @@ from functionModule import make_pizza as  mp
 print("Invoking the Function from the 'functionModule' File and Using it: ")
 functionModule.make_pizza(16, 'pepperoni')
 functionModule.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# using the '*' allows every function to be imported from the module but this time the dot operator doesn't need to be used, just the function name
+from functionModule import *
