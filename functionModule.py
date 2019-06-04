7652871161324cfb15a0a7662596c0375ed7f09c
Anish Senthilkumar <anishdsk@yahoo.com>
2019-06-04 14:23:42 -0400

    Module file example

---
+++ functionModule.py
@@ -0,0 +1,10 @@
+# FUNCTION MODULE
+
+# stores functions in seperate files to allow for reusability across multiple files
+
+# simulating making a pizza
+def make_pizza(size, *toppings):
+    """Summarizing the pizza to make"""
+    print("\nMaking a" + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("-" + topping)
---
+++ functionModuleCallingFile.py
@@ -0,0 +1,8 @@
+# CALLS THE FUNCTION FROM THE 'functionModule'  FILE
+
+# importing the module file
+import functionModule
+
+# invoke the function
+functionModule.make_pizza(16, 'pepperoni')
+functionModule.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
