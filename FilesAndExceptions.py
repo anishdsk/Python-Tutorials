3145c6fa7bd733e1b5ef27da195399ad7597b282
Anish Senthilkumar <anishdsk@yahoo.com>
2019-07-23 22:50:41 -0400

    comments changes

--- FilesAndExceptions.py
+++ FilesAndExceptions.py
@@ -1,6 +1,6 @@
 # FILES AND EXCEPTIONS
 
 print("Opening 'pi_digits.txt' and Reading/Printing its Contents:")
-with open('pi_digits.txt') as file_object:
+with open('pi_digits.txt') as file_object: # open() returns an object representing the file and the object is stored in 'file_object'
     contents = file_object.read() # read() leaves a blank line at the end of the output, use rstrip() to get rid of that line if needed
     print(contents.rstrip()) # this output will match exactly with the text file
