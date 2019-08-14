4a2bbdd7f8207aa3d972476ed318fadb200af167
Anish Senthilkumar <anishdsk@yahoo.com>
2019-08-13 20:09:34 -0400

    Writing and appending to files, try-except

--- FilesAndExceptions.py
+++ FilesAndExceptions.py
@@ -17,7 +17,7 @@ print("Open and Print Contents of 'pi_digits.txt' once more using the readLines(
 filename = 'pi_digits.txt'
 with open(filename) as file_object:
     lines = file_object.readlines() # readlines() method takes each line from file and stores it in a list, this list is then stored in 'lines'
-                                    # to be able to be worked with if file lines are needed past the 'with' block
+                                    # to be able to be worked with if file content is needed past the 'with' block
 for line in lines:
     print(line.rstrip())
 print()
@@ -33,3 +33,41 @@ print()
 
 ### NOTE: When Python reads from a text file it interprets all text in the file as a string, even numbers included.
 ### So if you want to work with a value in number form, convert it to an integer using int() or to a float using float()
+
+print("Using Large Files:")
+filename = 'pi_million_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ""
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string[:52] + "...")
+print(len(pi_string))
+print()
+
+print("Writing to a File:")
+filename = 'programming.txt'
+# second open() param says to open file in 'write mode' -> if file already exists then opening in this mode will erase file before returning file object first
+with open(filename, 'w') as file_object:
+    file_object.write("I love programming.")
+print()
+
+print("Writing Multiple Lines") # write() doesnt know when to create new line unless specified
+with open(filename, 'w') as file_object:
+    file_object.write("I love programming.\n") # '\n' is key here
+    file_object.write("I love creating new games.\n")
+print()
+
+print("Appending to a File:")
+filename = 'programming.txt'
+with open(filename, 'a') as file_object: # second open() param 'a' is used to append to the contents of a file rather than overwriting
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a brwoser.\n")
+
+print("Using 'try-except' Blocks:") # if an error occurs try-except blocks can handle the error to make sure the program keeps running
+try:
+    print(5/0)
+except ZeroDivisionError: # default error from python given whenever dividing by zero 
+    print("You can't divide by zero!")
